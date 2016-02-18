3f81131259a9003da0827fa27709382710ce5a38
Zihang Chen <chsc4698@gmail.com>
2016-02-18 00:43:46 -0500

    h

--- dist.sh
+++ dist.sh
@@ -3,9 +3,9 @@ mkdir $1_zc2324
 mkdir $1_zc2324/src
 mkdir $1_zc2324/include
 
-cp -r src/ $1_zc2324/src
-cp -r include/ $1_zc2324/include
-cp Makefile $1_zc2324/
+cp src/* $1_zc2324/
+cp include/* $1_zc2324/
+cp Makefile_ $1_zc2324/Makefile
 
 tar cf $1_zc2324.tar $1_zc2324
 
